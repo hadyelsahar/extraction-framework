004e56ba0ce5fd652efa5cb32bd249356105dfe3
hadyelsahar <hadyelsahar@gmail.com>
2013-06-30 00:03:26 +0200

    added script to generat specific iL links files

---
+++ scripts/src/main/bash/languageSpecificLinksGenerator.sh
@@ -0,0 +1,10 @@
+mkdir -p llinkfiles
+
+gawk < $1 '{
+match ($3 , /<http:\/\/(.*).dbpedia.org\/resource\/.*>/ ,m)
+{
+gsub("-","_",m[1])
+system("touch ./llinkfiles/interlanguage_links_same_as_" m[1] ".ttl")
+print $1,$2,$3 >> "./llinkfiles/interlanguage_links_same_as_" m[1] ".ttl"
+}
+}'
