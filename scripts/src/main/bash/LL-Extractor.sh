7c16791e7e0dc1078158974b33431e8ac6106797
hadyelsahar <hadyelsahar@gmail.com>
2013-07-12 05:25:14 +0200

    fix generated triples format

--- scripts/src/main/bash/LL-Extractor.sh
+++ scripts/src/main/bash/LL-Extractor.sh
@@ -1,5 +1,5 @@
 grep '<http://schema.org/about>' $1 |grep '.*\.wikipedia\.org\/wiki' | awk '{
-print $3,"<http://www.w3.org/2002/07/owl#sameAs>",$1
+print $3,"<http://www.w3.org/2002/07/owl#sameAs>",$1,"."
 }' |sed s/'.wikipedia.org\/wiki'/'.dbpedia.org\/resource'/  > languagelinks.nt
 
 
--- scripts/src/main/bash/languageSpecificLinksGenerator.sh
+++ scripts/src/main/bash/languageSpecificLinksGenerator.sh
@@ -5,6 +5,6 @@ match ($3 , /<http:\/\/(.*).dbpedia.org\/resource\/.*>/ ,m)
 {
 gsub("-","_",m[1])
 system("touch ./llinkfiles/interlanguage_links_same_as_" m[1] ".ttl")
-print $1,$2,$3 >> "./llinkfiles/interlanguage_links_same_as_" m[1] ".ttl"
+print $1,$2,$3,"." >> "./llinkfiles/interlanguage_links_same_as_" m[1] ".ttl"
 }
 }'
