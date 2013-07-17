383173d3e0f6adad1f402575776a95d8be9cfa04
hadyelsahar <hadyelsahar@gmail.com>
2013-07-17 22:43:26 +0200

    modified LLspecific bash script

--- scripts/src/main/bash/languageSpecificLinksGenerator.sh
+++ scripts/src/main/bash/languageSpecificLinksGenerator.sh
@@ -1,10 +1,22 @@
-mkdir -p llinkfiles
+#!/bin/sh
 
-gawk < $1 '{
+
+#mkdir -p llinkfiles
+filename=$1
+
+gawk < $filename '{
 match ($3 , /<http:\/\/(.*).dbpedia.org\/resource\/.*>/ ,m)
 {
+Q=$1
+lng=m[1]
+
 gsub("-","_",m[1])
 system("touch ./llinkfiles/interlanguage_links_same_as_" m[1] ".ttl")
 print $1,$2,$3,"." >> "./llinkfiles/interlanguage_links_same_as_" m[1] ".ttl"
+
 }
 }'
+
+
+
+
